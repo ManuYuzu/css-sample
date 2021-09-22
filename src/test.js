c3adca978d96a80d5bd14fbb5f92bfaa93ef6cc5
Manu Glez <manuaglez@gmail.com>
2021-09-22 21:52:29 +0100

    testing 2

--- src/test.js
+++ src/test.js
@@ -1,7 +1,7 @@
 const text = 'Texto de prueba'
 let i = 7
 
-const toPrint= function(value, number) {
+const toPrint = function(value, number) {
 
   while (i > 0) {
     console.log(`${text} nº: ${i}`)
